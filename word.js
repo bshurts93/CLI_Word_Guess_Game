58c90cb79add5a66871a5672f53cb4136ac09423
Ben Shurts <bshurts93@gmail.com>
2019-04-03 13:01:43 -0500

    Letter and Word constructors are able to work together to produce working functionality for building objects, displaying strings, and taking in guesses.

--- word.js
+++ word.js
@@ -1,29 +1,39 @@
 var Letter = require("./letter");
 
 
-function Word() {
-    this.arr = function (letters) {
-        var objArr = [];
+function Word(str) {
+    this.lettersArr = [];
+    this.arrGet = function () {
         // Create an arr of Letter objects for each letter of the array provided
-        letters.forEach(element => {
+        str.split("").forEach(element => {
             var letterObj = new Letter(element);
-            objArr.push(letterObj);
+            this.lettersArr.push(letterObj);
         });
-        return objArr;
     }
     this.string = function () {
-        // Returns a string representing the word
+        console.log("STRING FUNCTION");
+        var str = [];
 
         // Calls function in Letter obj "reveal" that displays either char or underscore
+        for (var i = 0; i < this.lettersArr.length; i++) {
+            str.push(this.lettersArr[i].reveal());
+        }
+        // Returns a string representing the word
+        return str.join("");
     };
     this.guess = function (char) {
         // Calls guess function from Letter object
+        this.lettersArr.forEach(element => {
+            element.check(char);
+        });
     };
 }
 
 
-var test = new Word();
-var testArr = "balls"
+var test = new Word("test");
 
+test.arrGet();
+console.log(test.lettersArr);
+test.guess("t");
+console.log(test.lettersArr);
 
-console.log(test.arr(testArr.split(""))); 
