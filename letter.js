4a15e1c3c112d8cfed8b4ed8f8afd46a3505d2d9
Ben Shurts <bshurts93@gmail.com>
2019-04-03 12:29:42 -0500

    Setup first function of Word constructor

--- letter.js
+++ letter.js
@@ -1,13 +1,20 @@
-module.exports = function Letter(char, bool) {
+module.exports = function Letter(char) {
     this.char = char;
-    this.bool = bool;
+    this.bool = false;
 
     this.reveal = function () {
         // Reveal the letter it was guessed
+        if (this.bool === true) {
+            return this.char;
+        } else {
+            return "_";
+        }
     }
     this.check = function (guess) {
         if (guess === this.char) {
             this.bool = true;
+            console.log("TRUE!");
         }
     }
 }
+
--- word.js
+++ word.js
@@ -1,18 +1,29 @@
 var Letter = require("./letter");
 
-var test = new Letter("A", true);
 
-
-
-console.log(test);
-
-
-function Word(arr) {
-    this.arr = arr;
+function Word() {
+    this.arr = function (letters) {
+        var objArr = [];
+        // Create an arr of Letter objects for each letter of the array provided
+        letters.forEach(element => {
+            var letterObj = new Letter(element);
+            objArr.push(letterObj);
+        });
+        return objArr;
+    }
     this.string = function () {
         // Returns a string representing the word
+
+        // Calls function in Letter obj "reveal" that displays either char or underscore
     };
     this.guess = function (char) {
         // Calls guess function from Letter object
     };
 }
+
+
+var test = new Word();
+var testArr = "balls"
+
+
+console.log(test.arr(testArr.split(""))); 
