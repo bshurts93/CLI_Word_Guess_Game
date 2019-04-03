1269ee52fa2be695734cacb8f53e30e93ab8eda9
Ben Shurts <bshurts93@gmail.com>
2019-04-03 11:30:51 -0500

    Defined the Word constructor

--- letter.js
+++ letter.js
@@ -1,12 +1,13 @@
 module.exports = function Letter(char, bool) {
     this.char = char;
     this.bool = bool;
+
+    this.reveal = function () {
+        // Reveal the letter it was guessed
+    }
     this.check = function (guess) {
         if (guess === this.char) {
             this.bool = true;
         }
-    }
-    this.reveal = function () {
-        // Reveal the letter it was guessed
     }
 }
--- word.js
+++ word.js
@@ -5,3 +5,14 @@ var test = new Letter("A", true);
 
 
 console.log(test);
+
+
+function Word(arr) {
+    this.arr = arr;
+    this.string = function () {
+        // Returns a string representing the word
+    };
+    this.guess = function (char) {
+        // Calls guess function from Letter object
+    };
+}
